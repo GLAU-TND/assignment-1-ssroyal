7c458008bd2cf658ae1f890de9fb2bc35ee290ce
saurabh <ssroyal>
2020-02-20 00:17:00 +0530

    add MyContacts class

---
+++ src/Definition/MyContacts.java
@@ -0,0 +1,32 @@
+package Definition;
+
+import Adt.MyContactsAdt;
+
+import java.util.Scanner;
+
+public class MyContacts implements MyContactsAdt {
+    Scanner sc = new Scanner(System.in);
+
+    @Override
+    public void addContact() {
+
+
+    }
+
+    @Override
+    public void viewContacts() {
+
+    }
+
+    @Override
+    public Object deletecontact() {
+        return null;
+    }
+
+    @Override
+    public void searchcontact() {
+
+    }
+
+
+}
