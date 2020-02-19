789d7f6ca7beb73cbd151d0a5cb19c600a35c689
saurabh <ssroyal>
2020-02-19 21:11:49 +0530

    add node class

--- src/Definition/MyList.java
+++ src/Definition/MyList.java
@@ -1,5 +1,22 @@
 package Definition;
 
 public class MyList {
+    private static  class Node<E>
+    {
+        private E data;
+        private Node<E> next;
 
+        private Node(E data, Node<E> next) {
+            this.data = data;
+            this.next = next;
+        }
+
+        public E getData() {
+            return data;
+        }
+
+        public Node<E> getNext() {
+            return next;
+        }
+    }
 }
