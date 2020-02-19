0d5f4a0454f69e53631d937fe3083f7d6b1d4570
saurabh <ssroyal>
2020-02-20 00:54:58 +0530

    add template of Main Class

---
+++ src/Execution/Main.java
@@ -0,0 +1,21 @@
+package Execution;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        boolean response = false;
+        do {
+            System.out.println("Welcome to Saurabh's Contact List App\n" +
+                    "Press 1 to add a new contact\n" +
+                    "Press 2 to view all contacts\n" +
+                    "Press 3 to search for a contact\n" +
+                    "Press 4 to delete a contact\n" +
+                    "Press 5 to exit program ");
+
+        } while (response);
+
+
+    }
+}
